d761c24d7929d40acb024bf0ca9850b84e5f3fec
ruyisbaros <ahmet.erdonmez77@gmail.com>
2022-09-30 23:05:54 +0200

    Register UI tested. result ok!

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { loggingFail, loggingStart, loggingSuccess } from "../redux/authSlicer";
 
 const Register = () => {
   const navigate = useNavigate();
@@ -10,6 +13,7 @@ const Register = () => {
   const [passType, setPassType] = useState(false);
   const [confPassType, setConfPassType] = useState(false);
   const [errText, setErrText] = useState("");
+  const [passwordsMatch, setPasswordsMatch] = useState(true);
   const [user, setUser] = useState({
     name: "",
     email: "",
@@ -19,20 +23,63 @@ const Register = () => {
     imageId: "pw4gq42vstslcyqzi81o",
     cf_password: "",
   });
-  const { email, password, name, about, cf_password } = user;
+  const { email, password, name, about, roles, imageId, cf_password } = user;
+  // console.log(roles);
+
+  useEffect(() => {
+    if (password !== cf_password) {
+      setPasswordsMatch(false);
+    } else {
+      setPasswordsMatch(true);
+    }
+  }, [cf_password]);
+
+  const [error, setError] = useState({
+    errors: {},
+    isError: false,
+  });
 
   const handleInput = (e) => {
     setUser({ ...user, [e.target.name]: e.target.value });
   };
+  //console.log(user);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (password === cf_password) {
+      try {
+        dispatch(loggingStart());
+        const { data } = await axios.post("/api/v1/auth/register", {
+          email,
+          password,
+          name,
+          about,
+          roles,
+          imageId,
+        });
+        console.log(data);
+        setPasswordsMatch(true);
+        dispatch(loggingSuccess());
+        toast.success("You have been registered successufully...");
+      } catch (error) {
+        dispatch(loggingFail());
+        //console.log(error);
+        //console.log(error.response.data.message);
+        toast.error(error.response.data.message);
+        setErrText(error.response.data.message);
+        //console.log(errText.split(" "));
+      }
+    } else {
+      dispatch(loggingFail());
+      toast.error("Passwords don't match!");
+    }
   };
 
   return (
     <div className="auth_page">
       <form onSubmit={handleSubmit}>
-        <h3 className="text-capitalize text-center mb-5">
+        <h3 className="text-capitalize text-center my-2">
           Welcome to <br />
           <span style={{ color: "blue", fontWeight: "bold" }}>
             Blogging <br />
@@ -56,6 +103,9 @@ const Register = () => {
         <div className="form-group">
           <label htmlFor="email">Email Address</label>
           <input
+            style={{
+              backgroundColor: errText.split(" ").includes("email:") && "red",
+            }}
             type="email"
             name="email"
             className="form-control"
@@ -108,6 +158,14 @@ const Register = () => {
           <label htmlFor="cf_password">Confirm Password</label>
           <div className="pass">
             <input
+              style={{
+                backgroundColor:
+                  password && cf_password
+                    ? !passwordsMatch
+                      ? "red"
+                      : "green"
+                    : "inherit",
+              }}
               type={confPassType ? "text" : "password"}
               name="cf_password"
               className="form-control"
